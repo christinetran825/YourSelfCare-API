c9250e0007ed1d3597e96507fb2a78aca72893d9
github username <github email address>
2018-03-24 00:03:41 -0700

    changed type_of to condition

--- app/controllers/api/condition_controller.rb
+++ app/controllers/api/condition_controller.rb
@@ -1,34 +1,34 @@
-class Api::TypeOfController < ApplicationController
+class Api::ConditionController < ApplicationController
 
-  before_action :set_type_of, only: [:show, :update, :destroy]
+  before_action :set_condition, only: [:show, :update, :destroy]
 
   def index
-    render json: TypeOf.all
+    render json: Condition.all
   end
 
   def create
-    type_of = TypeOf.new(type_of_params)
-    if type_of.save
-      render json: type_of
+    condition = Condition.new(condition_params)
+    if condition.save
+      render json: condition
     else
-      render json: { message: type_of.errors }, status: 400
+      render json: { message: condition.errors }, status: 400
     end
   end
 
   def show
-    render json: @type_of
+    render json: @condition
   end
 
   def update
-    if @type_of.update(type_of_params)
-      render json: @type_of
+    if @condition.update(condition_params)
+      render json: @condition
     else
-      render json: { message: @type_of.errors }, status: 400
+      render json: { message: @condition.errors }, status: 400
     end
   end
 
   def destroy
-    if @type_of.destroy
+    if @condition.destroy
       render json: { message: "Successfully removed item." }, status: 204
     else
       render json: { message: "Unable to remove item" }, status: 400
@@ -37,12 +37,12 @@ class Api::TypeOfController < ApplicationController
 
   private
 
-    def set_type_of
-      @type_of = TypeOf.find_by(id: params[:id])
+    def set_condition
+      @condition = Condition.find_by(id: params[:id])
     end
 
-    def type_of_params
-      params.require(:type_of).permit(:name)
+    def condition_params
+      params.require(:condition).permit(:name)
     end
 
 end
