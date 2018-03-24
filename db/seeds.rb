ff41b5dd273dc709aa72bff962520d43797ba7ed
github username <github email address>
2018-03-24 00:01:24 -0700

    redefined code for behavior

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,52 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+behavior_list = [
+  ["Sleep", "Sleep Details"],
+  ["Mood", "Mood Details"],
+  ["Energy", "Energy Details"],
+  ["Mental", "Mental Details"],
+  ["Drinks", "Drinks Details"],
+  ["Medication", "Medication Details"],
+]
 
-behavior = Behavior.create([{ name: "Sleep", details: "Sleep Details", type_of: ["Good", "OK", "Bad"] }, { name: "Mood", details: "Mood Details", type_of: ["Happy", "Sad", "Anxious", "Sensitive", "Hopeless", "Angry", "Irritable", "Empty", "Tired"] }, { name: "Energy", details: "Energy Details", type_of: ["Very High", "High", "Low", "Very Low"] }, { name: "Mental", details: "Mental Details", type_of: ["Calm", "Stressed", "Distracted", "Focused"] }, { name: "Drinks", details: "Drinks Details", type_of: ["Water", "Coffee", "Tea", "Alcohol"] }, { name: "Medication", details: "Medication Details", type_of: [""] }])
+behavior_list.each do |behavior|
+  Behavior.create( name: behavior[0], details: behavior[1])
+end
 
+# behavior_list = [
+#   ["Sleep", "Sleep Details", ["Good", "OK", "Bad"]],
+#   ["Mood", "Mood Details", ["Happy", "Sad", "Anxious", "Sensitive", "Hopeless", "Angry", "Irritable", "Empty", "Tired"]],
+#   ["Energy", "Energy Details", ["Very High", "High", "Low", "Very Low"]],
+#   ["Mental", "Mental Details", ["Calm", "Stressed", "Distracted", "Focused"]],
+#   ["Drinks", "Drinks Details", ["Water", "Coffee", "Tea", "Alcohol"]],
+#   ["Medication", "Medication Details", [""]],
+# ]
+#
+# behavior_list.each do |behavior|
+#   Behavior.create( name: behavior[0], details: behavior[1], conditions_attributes: behavior[2])
+# end
+
+# condition_list = [
+#   ["Good", "OK", "Bad"],
+#   ["Happy", "Sad", "Anxious", "Sensitive", "Hopeless", "Angry", "Irritable", "Empty", "Tired"],
+#   ["Very High", "High", "Low", "Very Low"],
+#   ["Calm", "Stressed", "Distracted", "Focused"],
+#   ["Water", "Coffee", "Tea", "Alcohol"],
+#   [""]
+# ]
 
 # Behaviors {
-#   [ Sleep {
-#       [Good,
-#        OK,
-#        Bad]
+#   [ name: "Sleep",
+#     details: "Sleep Details",
+#     conditions: {
+#       [ { id: 0, name: "Good" },
+#        { id: 1, name: "OK" },
+#        { id: 2, name: "Bad" }]
 #     },
-#     Mood {
+#     name: "Mood",
+#     details: "Mood Details",
+#     conditions: {
 #       [ { id: 0, name: "Happy" },
 #        { id: 1, name: "Sad" },
 #        { id: 2, name: "Anxious" },
@@ -28,27 +63,35 @@ behavior = Behavior.create([{ name: "Sleep", details: "Sleep Details", type_of: 
 #        { id: 8, name: "Tired" },
 #       ]
 #     },
-#     Energy {
+#     name: "Energy",
+#     details: "Energy Details",
+#     conditions: {
 #       [ { id: 0, name: "Very High" },
 #        { id: 1, name: "High" },
 #        { id: 2, name: "Low" },
 #        { id: 3, name: "Very Low" },
 #     },
-#     Mental {
+#     name: "Mental",
+#     details: "Mental Details",
+#     conditions: {
 #       [ { id: 0, name: "Calm" },
 #         { id: 1, name: "Stressed" },
 #         { id: 2, name: "Distracted" },
 #         { id: 3, name: "Focused" },
 #       ]
 #     },
-#     Drinks {
+#     name: "Drinks",
+#     details: "Drinks Details",
+#     conditions: {
 #       [ { id: 0, name: "Water" },
 #         { id: 1, name: "Coffee" },
 #         { id: 2, name: "Tea" },
 #         { id: 3, name: "Alcohol" },
 #       ]
 #     },
-#     Medication {
+#     name: "Medication",
+#     details: "Medication Details",
+#     conditions: {
 #       [ { id: 0, name: "", dose: integer }]
 #     }
 #   ]
