410e57dee63264a9b2ba4f474e8f1e6b39a046d1
github username <github email address>
2018-03-26 21:20:06 -0700

    updated behavior details

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 behavior_list = [
-  ["Sleep", "Sleep Details"],
-  ["Mood", "Mood Details"],
-  ["Energy", "Energy Details"],
-  ["Mental", "Mental Details"],
-  ["Drinks", "Drinks Details"],
-  ["Medication", "Medication Details"],
+  ["Sleep", "Sleep and Energy go hand in hand. Erratic sleep patterns can affect a person's energy, mood, and mental health."],
+
+  ["Mood", "A mood disorder is a mental health class that health professionals use to broadly describe all types of depression and bipolar disorders. Depending on age and the type of mood disorder, a person may have different symptoms of depression. "],
+
+  ["Energy", "Energy or Fatigue levels can change depending on the level of depression. It can affect a person's sleep."],
+
+  ["Mental", "Mental or Cognitive distortions are ways that our mind convinces us of something that isn't true. Inaccurate thoughts reinforces the negative thoughts and emotions/moods. At times, how an environment or a single event a person experiences can affect these negative thoughts."],
+
+  ["Drinks", "It's important to drink the daily recommended amount of water. Drinking caffeinated liquids like tea and coffee can worsen anxiety and possibly worsen depression by causing headaches, fatigue, and irritability. Alcohol can lead to self-medicating symptoms and worsening the depression."],
+  
+  ["Medication", "When taken as prescribed, medications can help manage a person's symptoms. If a person comes off their medicine too soon, or if they feel better, it can lead to a relapse. Medication will not always be the first choice, especially if your depression is mild. There are a number of different types of antidepressants available. Your GP can explain which they believe is the best for you and why. What your doctor prescribes will depend on the type and severity of depression you have. If you experience problems from your medicine or have any concerns, speak to your GP."],
 ]
 
 behavior_list.each do |behavior|
