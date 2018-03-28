828b6088a9e8a9ccfb336417f71276bbd553c414
github username <github email address>
2018-03-27 21:40:29 -0700

    added users route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    resources :behaviors
+    resources :behaviors, :users
   end
 end
