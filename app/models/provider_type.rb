d55527d5890c2ea09bc0a8ed8c3f9fb5ee7b07db
github username <github email address>
2018-03-28 19:32:58 -0700

    removed all files of the model to create a new model with proper naming

--- app/models/provider_type.rb
+++
@@ -1,4 +0,0 @@
-class ProviderType < ApplicationRecord
-  has_many :providers_provider_types
-  has_many :providers, through: :provider_providers_types
-end
--- db/migrate/20180328033017_create_provider_types.rb
+++
@@ -1,9 +0,0 @@
-class CreateProviderTypes < ActiveRecord::Migration[5.1]
-  def change
-    create_table :provider_types do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
