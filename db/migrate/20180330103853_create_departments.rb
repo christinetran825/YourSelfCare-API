00fbca8d53c976c43a18d4be4f52e53064f553aa
github username <github email address>
2018-03-30 12:49:08 -0700

    removed index/references of provider id

--- db/migrate/20180330103853_create_departments.rb
+++ db/migrate/20180330103853_create_departments.rb
@@ -2,7 +2,6 @@ class CreateDepartments < ActiveRecord::Migration[5.1]
   def change
     create_table :departments do |t|
       t.string :name
-      t.integer :provider_id
 
       t.timestamps
     end
