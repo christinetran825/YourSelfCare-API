eaeeef3ce7d978eeb11194610b73d0d2ec832c4a
github username <github email address>
2018-03-30 13:09:25 -0700

    added department to provider association

---
+++ db/migrate/20180330200741_add_department_to_provider.rb
@@ -0,0 +1,5 @@
+class AddDepartmentToProvider < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :providers, :department, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330103853) do
+ActiveRecord::Schema.define(version: 20180330200741) do
 
   create_table "behavior_condition", force: :cascade do |t|
     t.integer "behavior_id"
@@ -38,9 +38,10 @@ ActiveRecord::Schema.define(version: 20180330103853) do
 
   create_table "departments", force: :cascade do |t|
     t.string "name"
-    t.integer "provider_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "provider_id"
+    t.index ["provider_id"], name: "index_departments_on_provider_id"
   end
 
   create_table "insurances", force: :cascade do |t|
@@ -75,6 +76,8 @@ ActiveRecord::Schema.define(version: 20180330103853) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "department_id"
+    t.index ["department_id"], name: "index_providers_on_department_id"
     t.index ["user_id"], name: "index_providers_on_user_id"
   end
 
