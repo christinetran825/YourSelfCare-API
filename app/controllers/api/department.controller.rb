94eba2eaedcc068ae1374ad691699c3239d7efe7
github username <github email address>
2018-03-30 03:43:38 -0700

    updated spellings

--- app/controllers/api/department.controller.rb
+++ app/controllers/api/department.controller.rb
@@ -1,18 +1,17 @@
-class Api::Departments< ApplicationController
+class Api::DepartmentController< ApplicationController
 
-  before_action :set_departments, only: [:show, :update, :destroy]
+  before_action :set_department, only: [:show, :update, :destroy]
 
   def index
-    @departments = @user.providers.departments
-    render json: @departments
+    render json: Department.all
   end
 
   def create
-    @department = @user.providers.department.build(departments_params)
-    if @department && @provider.save
+    @department = Department.new(department_params)
+    if @department && @department.save
       render json: @department
     else
-      render json: { message: provider.errors }, status: 400
+      render json: { message: @department.errors }, status: 400
     end
   end
 
@@ -38,12 +37,57 @@ class Api::Departments< ApplicationController
 
   private
 
-    def set_departments
+    def set_department
       @department = Department.find_by(id: params[:id])
     end
 
-    def departments_params
-      params.require(:departments).permit(:name)
+    def department_params
+      params.require(:department).permit(:name)
     end
 
 end
+
+#
+# def index
+#   @departments = @user.providers.departments
+#   render json: @departments
+# end
+#
+# def create
+#   @department = @user.providers.department.build(departments_params)
+#   if @department && @department.save
+#     render json: @department
+#   else
+#     render json: { message: provider.errors }, status: 400
+#   end
+# end
+#
+# def show
+#   render json: @department
+# end
+#
+# def update
+#   if @department.update(department_params)
+#     render json: @department
+#   else
+#     render json: { message: @department.errors }, status: 400
+#   end
+# end
+#
+# def destroy
+#   if @department.destroy
+#     render json: { message: "Successfully removed item." }, status: 204
+#   else
+#     render json: { message: "Unable to remove item" }, status: 400
+#   end
+# end
+#
+# private
+#
+#   def set_departments
+#     @department = Department.find_by(id: params[:id])
+#   end
+#
+#   def departments_params
+#     params.require(:departments).permit(:name)
+#   end
