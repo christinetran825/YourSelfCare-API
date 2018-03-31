6d8d7093d8baeb5aa4d69c7210da0445df450956
github username <github email address>
2018-03-30 20:46:43 -0700

    removed comments

--- app/controllers/api/department.controller.rb
+++ app/controllers/api/department.controller.rb
@@ -45,49 +45,5 @@ class Api::DepartmentController< ApplicationController
       params.require(:department).permit(:name)
     end
 
-end
 
-#
-# def index
-#   @departments = @user.providers.departments
-#   render json: @departments
-# end
-#
-# def create
-#   @department = @user.providers.department.build(departments_params)
-#   if @department && @department.save
-#     render json: @department
-#   else
-#     render json: { message: provider.errors }, status: 400
-#   end
-# end
-#
-# def show
-#   render json: @department
-# end
-#
-# def update
-#   if @department.update(department_params)
-#     render json: @department
-#   else
-#     render json: { message: @department.errors }, status: 400
-#   end
-# end
-#
-# def destroy
-#   if @department.destroy
-#     render json: { message: "Successfully removed item." }, status: 204
-#   else
-#     render json: { message: "Unable to remove item" }, status: 400
-#   end
-# end
-#
-# private
-#
-#   def set_departments
-#     @department = Department.find_by(id: params[:id])
-#   end
-#
-#   def departments_params
-#     params.require(:departments).permit(:name)
-#   end
+end
