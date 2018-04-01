b4ec209b066324ed8671510f6cb867bef837889b
github username <github email address>
2018-04-01 00:16:06 -0700

    moved post user_token within namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
     resources :behaviors, :users
+    post 'user_token' => 'user_token#create'
   end
 end
