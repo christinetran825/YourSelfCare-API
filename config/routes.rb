e17af0f71cac05610b7aa8a09914ab4f454d5c89
github username <github email address>
2018-04-06 22:58:39 -0700

    added conditions to namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # resources :behaviors
+  # resources :users
+  # resources :medications
+  # resources :insurances
+  # resources :providers
+  # resources :departments
+
   namespace :api do
-    resources :behaviors, :users
+    resources :behaviors, :conditions, :users, :medications, :insurances, :providers, :departments
   end
 end
