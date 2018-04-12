2768524dd7591f674a61a7c3b56277e701275c16
github username <github email address>
2018-04-11 22:41:59 -0700

    added show routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,12 @@ Rails.application.routes.draw do
   namespace :api do
     resources :behaviors, :conditions, :users, :medications, :insurances, :providers, :departments
   end
+
+  get '/api/users/:user', :to => 'api/users#show'
+  get '/api/medications/:medication', :to => 'api/medications#show'
+  get '/api/insurances/:insurance', :to => 'api/insurances#show'
+  get '/api/providers/:provider', :to => 'api/providers#show'
+  get '/api/behaviors/:behavior', :to => 'api/behaviors#show'
+  get '/api/conditions/:condition', :to => 'api/conditions#show'
+
 end
