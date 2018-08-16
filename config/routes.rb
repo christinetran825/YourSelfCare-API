1cbf1953448d0e27c8721096e5f121e1beab1546
github username <github email address>
2018-08-16 11:04:04 -0700

    cleaned up file

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
-  post 'find_user' => 'users#find'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # resources :behaviors
-  # resources :users
-  # resources :medications
-  # resources :insurances
-  # resources :providers
-  # resources :departments
 
   namespace :api do
     resources :users, :medications, :insurances
+    post 'user_token' => 'user_token#create'
+    get 'find_user' => 'users#find'
   end
 
   namespace :api do
@@ -32,7 +25,5 @@ Rails.application.routes.draw do
   get '/api/providers/:provider', :to => 'api/providers#show'
   get '/api/behaviors/:behavior', :to => 'api/behaviors#show'
   get '/api/conditions/:condition', :to => 'api/conditions#show'
-
   get '/api/providers/:provider_id/departments', :to => 'api/departments#index'
-
 end
