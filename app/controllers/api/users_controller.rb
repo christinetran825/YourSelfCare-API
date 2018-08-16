ed4228895d08bb72b3f8c7a942dca7ecd6d1a365
github username <github email address>
2018-08-16 11:05:53 -0700

    uncommented before actions; changed password_digest to password in user params

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,7 +10,7 @@ class Api::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user && @user.save
+    if @user.valid? && @user.save
       render json: @user
     else
       render json: @user.errors, status: 400
@@ -33,6 +33,16 @@ class Api::UsersController < ApplicationController
     @user.destroy
   end
 
+  def find
+   @user = User.find_by(email: params[:user][:email])
+   if @user
+     render json: @user
+   else
+     @errors = @user.errors.full_messages
+     render json: @errors
+   end
+  end
+
   private
 
     def set_user
@@ -40,7 +50,7 @@ class Api::UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest)
+      params.require(:user).permit(:name, :email, :password)
     end
 
 end
