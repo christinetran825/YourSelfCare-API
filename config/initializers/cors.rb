57090b278e1f94be12bb44a14d4da7a3398bd3da
github username <github email address>
2018-09-14 15:19:59 -0700

    readded localhost3000; removed last slash in surge domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://selfcare.surge.sh'
+    origins 'localhost:3000', 'https://selfcare.surge.sh'
 
     resource '*',
       headers: :any,
